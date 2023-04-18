aefc0346c5e962f8707751138be2f6d55ccb3fb5
Mark S <mark.schlachter@technologyadvice.com>
2023-04-18 12:07:36 -0400

    feat: Add retry-related error type(s)

--- src/errors.rs
+++ src/errors.rs
@@ -14,10 +14,25 @@ pub enum BriteVerifyClientError {
     /// instance
     #[error("No BriteVerify API key provided")]
     MissingApiKey,
+    /// A request cannot be "built" for sending
+    #[error("Request cannot be built!")]
+    UnbuildableRequest(#[from] reqwest::Error),
+    /// A request cannot be cloned when automatic
+    /// rate-limit retry is enabled
+    #[error("Request cannot be cloned for retry!")]
+    UnclonableRequest,
     /// Invalid or unusable API key provided when constructing
     /// a [`BriteVerifyClient`][crate::BriteVerifyClient] instance
     #[error(transparent)]
     InvalidHeaderValue(#[from] InvalidHeaderValue),
+    /// Invalid or unusable base url provided when constructing
+    /// a [`BriteVerifyClient`][crate::BriteVerifyClient] instance
+    #[error(transparent)]
+    InvalidBaseUrl(#[from] http::uri::InvalidUri),
+    /// The BriteVerify API returned an unusable response
+    /// (based on HTTP status code)
+    #[error("Unusable (non-2xx) response")]
+    UnusableResponse(reqwest::Response),
     /// A catch-all error for any other errors encountered
     #[error(transparent)]
     Other(#[from] anyhow::Error),
